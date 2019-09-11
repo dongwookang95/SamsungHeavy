403d9ced0626b9eed43229ac16c119e6dbb1d728
dongwookang <d.kang-1@student.tudelft.nl>
2019-09-11 18:40:53 +0200

    implement check the rows of a file

--- ReadFile.java
+++ ReadFile.java
@@ -6,11 +6,30 @@ public class ReadFile{
     int row = 0;
     String [][] items;
 
-    //check correct path
-
+    //check correct path and get file
     public boolean checkFile(){
         return file.isFile();
     }
+    //check number of rows in csv file
+    public int checkRowNumber(){
+        row = 0;
+        if(checkFile()==true){
+            //do if it is file
+            try{
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                while((reader.readLine())!=null){
+                    row++;
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+
+        } else{
+            System.out.println("This is not a file");
+        }
+        return row;
+    }
+
 
 
 }
--- StockMarket.java
+++ StockMarket.java
@@ -2,5 +2,6 @@ public class StockMarket{
     public static void main(String[] args){
         ReadFile r = new ReadFile();
         System.out.println(r.checkFile());
+        System.out.println(r.checkRowNumber());
     }
 }
